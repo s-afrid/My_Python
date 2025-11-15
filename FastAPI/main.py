75ce30914f10ba8a39dbf648f858d113b701f101
s-afrid <afrid0052@gmail.com>
2025-11-15 15:02:50 +0530

    serving static and dynamic pages

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,11 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from typing import Union
 
 app = FastAPI()
 
-@app.get('/')
-def read_root():
-    return {"Hello":"World"}
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get('/',response_class=HTMLResponse)
+async def read_home(request: Request):
+    return templates.TemplateResponse('index.html',{'request': request})
+
+@app.get("/page/{id}", response_class=HTMLResponse)
+async def read_page(request: Request, id: str):
+    return templates.TemplateResponse(
+        request=request, name="page.html", context={"id": id}
+    )
 
 @app.get('/items/{item_id}')
 def read_item(item_id: int, q: Union[str, None] = None):
